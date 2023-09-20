dac95e655f0be57545a7f140c0c708d64fde01ae
Ethar <etharabdelazeem1@gmail.com>
2023-09-20 21:00:32 +0200

    Update promptfun.c

--- promptfun.c
+++ promptfun.c
@@ -1,18 +1,11 @@
 #include "shell.h"
 
 /**
- * prompt - Display Shell Prompt
- */
-void prompt(void)
-{
-	PRINTER("$ ");
-}
-/**
- * print_error - Display Error Based on Command and How Many Time Shell Looped
- * @input:User Input
- * @counter:Simple Shell Count Loop
- * @argv:Program Name
- * Return: Void
+ * print_error - prints the error messages 
+ * @input:the input receivedx from the user
+ * @counter: the counter of lines
+ * @argv:the arguments vector
+ * Return: Nothing
  */
 void print_error(char *input, int counter, char **argv)
 {
@@ -26,4 +19,43 @@ void print_error(char *input, int counter, char **argv)
 	PRINTER(": ");
 	PRINTER(input);
 	PRINTER(": not found\n");
+}
+
+/**
+ * prompt - prints out the prompt message
+ * Return: Nothing
+ */
+void prompt(void)
+{
+	PRINTER("$ ");
+}
+
+/**
+ * check_builtin - check builtin
+ *
+ * @cmd:the entered command
+ * Return: Zero upon success and -1 otherwise
+ */
+int is_builtin(char **comd)
+{
+	bul_t fun[] = {
+		{"cd", NULL},
+		{"help", NULL},
+		{"echo", NULL},
+		{"history", NULL},
+		{NULL, NULL}
+	};
+	int i = 0;
+		if (*comd == NULL)
+	{
+		return (-1);
+	}
+
+	while ((fun + i)->command)
+	{
+		if (_strcmp(comd[0], (fun + i)->command) == 0)
+			return (0);
+		i++;
+	}
+	return (-1);
 }
