957e3aefcb2f19880a54f5cbba2569ec78d729d7
Ethar <etharabdelazeem1@gmail.com>
2023-09-20 21:05:11 +0200

    Update and rename printer.c to print_number.c

---
+++ print_number.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * print_number_in - prints a number
+ * @n:the integer to be printed
+ * Return:Nothing
+ */
+void print_number_in(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
+
+
+
+/**
+ * print_number - prints a number
+ * @n: the unsigned integer
+ * Return: Nothing
+ */
+void print_number(unsigned int n)
+{
+	unsigned int x = n;
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
